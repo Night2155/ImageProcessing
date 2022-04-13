570448bd759ca6515df02c9385c3ed204410c61a
09765 <4a8g0034@stust.edu.tw>
2022-04-13 20:55:44 +0800

    Homework V2

--- main.py
+++ main.py
@@ -8,15 +8,16 @@ from ClassFile import Image_20220317
 
 imgpath = "image\\fox_in_snow.jpg"
 oriimg = cv.imread(imgpath)
+guassion = cv.GaussianBlur(oriimg, (5, 5), 0)
 #Image_20220317.my_first_kernel(oriimg)
-Image_20220317.gussian_filter(oriimg)
-Image_20220317.median_filter(oriimg)
+#Image_20220317.gussian_filter(oriimg)
+#Image_20220317.median_filter(oriimg)
 #Image_20220317.sobel_filter(oriimg)
 #imgCrop = cv.cvtColor(oriimg, cv.COLOR_BGR2HSV)
 #gray = cv.cvtColor(oriimg, cv.COLOR_BGR2GRAY)
 #cv.imshow("Thrsehold", ImageProcess.Thresholding(oriimg, 127))
 #ImageProcess.Show_Histogram(gray)
 #ImageProcess.Show_Histogram(oriimg)
-#cv.imshow("sdad", imgCrop)
+cv.imshow("sdad", guassion)
 cv.waitKey(0)
 
